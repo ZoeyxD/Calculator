78c964a77f97cba831e20bde5a7694c7b279445b
ZoeyxD <142680830+ZoeyxD@users.noreply.github.com>
2023-09-15 23:27:51 +0200

    Functioning calculator

--- calc/src/app/calc/calc.component.ts
+++ calc/src/app/calc/calc.component.ts
@@ -6,24 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./calc.component.css']
 })
 export class CalcComponent {
-  public writeme: string = "";
+  public display: string = "";
+  public nmb: string = "";
   public tmp: number = 0;
+  public tmpFirst: number = 0;
+  public operator: string = "";
 
+  // saves clicked number 
   public saveme(x: string) {
-    if (x == "+") {
-      this.tmp = this.tmp + Number(this.writeme);
-      this.writeme = "";
-      return;
-    }
-    this.writeme = this.writeme + x;
+    this.nmb = this.nmb + x;
+    this.tmp = Number(this.nmb); 
+    this.display = this.display + x;
+
   }
+
+  // saves operator & resets the clicked number 
+  public saveop(x: string) {
+    if (x == "+" || "-" || "/" || "*") {
+      this.tmpFirst = this.tmp; 
+      this.nmb = "";
+      this.tmp = 0;
+      this.operator = x;
+      this.display = this.display + x;
+    }
+  }
+  
+  public result() {
+    if (this.operator === "+"){
+      this.tmp = this.tmpFirst + this.tmp
+      this.display = String(this.tmp);
+      
+    } else if (this.operator === "-"){
+      this.tmp = this.tmpFirst - this.tmp
+      this.display = String(this.tmp);
+    
+    } else if (this.operator === "*"){
+        this.tmp = this.tmpFirst * this.tmp
+        this.display = String(this.tmp);
+      
+    } else if (this.operator === "/" && this.tmp != 0){
+      this.tmp = this.tmpFirst / this.tmp
+      this.display = String(this.tmp);
+
+    } else this.display = "Cannot divide by zero"
+    
+  } 
 
   public delete() {
-    this.writeme = "";
+    this.display = "";
+    this.tmp = 0;
+    this.nmb = "";
   }
 
-  public result() {
-    this.tmp = this.tmp + Number(this.writeme);
-    this.writeme = String(this.tmp);
-  }
 }
